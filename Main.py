686ffcd39de58512a10f17178e934b969043caba
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-10 18:47:03 +0700

    Feat: Added a command to select crypto and refresh it every second it is entered.

--- Main.py
+++ Main.py
@@ -1,15 +1,34 @@
-from Utils.Utility import Input, Color, Center, CRYPTO_LIST, BASE_URL, COINGECKO_API_KEY
+import time
+from Utils.Utility import Input, Color, Center
 from Core.Data_fetcher import fetch_current_price, fetch_historical_prices
 from Core.Processor import analyze_signal
 
+CRYPTO_LIST = [
+    "bitcoin",
+    "ethereum",
+    "solana",
+    "binancecoin"
+]
 
-current_price = fetch_current_price(CRYPTO_LIST)
-historical_prices = fetch_historical_prices(CRYPTO_LIST, days=7)
-signal, avg_price, diff = analyze_signal(current_price, historical_prices)
+print(Center.box("CRYPTO SIGNAL ANALYZER"))
+for i, coin in enumerate(CRYPTO_LIST, 1):
+    print(f"{i}. {coin}")
 
+choice = int(Input.number("\nPilih Crypto (1-4): "))
+coin_id = CRYPTO_LIST[choice - 1]
 
-print(f"Crypto         : {CRYPTO_LIST[0]}")
-print(f"Current Price : {Color.Green('$')}"+f"{Color.Green(current_price)}")
-print(f"7D Average    : ${avg_price:.2f}")
-print(f"Signal        : {signal}")
-print(f"Diff          : {diff:.2f}%")
+refresh = int(input("Resfresh tiap: "))
+
+while True:
+    current_price = fetch_current_price(coin_id)
+    historical_prices = fetch_historical_prices(coin_id, days=30)
+    
+    signal, avg_price, diff = analyze_signal(current_price, historical_prices)
+    
+    print("\n" + Center.box("RESULT"))
+    print(f"Crypto         : {coin_id}")
+    print(f"Current Price  : {Color.Green('$')}{Color.Green(current_price)}")
+    print(f"7D Average     : ${avg_price:.2f}")
+    print(f"Signal         : {signal}")
+    print(f"Diff           : {diff:.2f}%")
+    time.sleep(refresh)
