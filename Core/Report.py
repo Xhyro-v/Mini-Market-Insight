a4b50d1cc365653eb44e89f7d50c7f47eda0e806
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-12 18:15:32 +0700

    Feat: Simplify report output

--- Core/Report.py
+++ Core/Report.py
@@ -1,1 +1,19 @@
+from Utils.Utility import Color, Center, Font
 
+class Output:
+    @staticmethod
+    def report(coin, current, avg, signal, diff):
+        print("\n" + Center.box("CRYPTO REPORT"))
+
+        print(f"{Font.Bold('Crypto')}        : {coin}")
+        print(f"{Font.Bold('Current Price')} : {Color.Green('$')}{Color.Green(f'{current:,.2f}')}")
+        print(f"{Font.Bold('Avg (30D)')}     : ${avg:,.2f}")
+        print(f"{Font.Bold('Signal')}        : {signal}")
+
+        if diff >= 0:
+            diff_text = Color.Green(f"+{diff:.2f}%")
+        else:
+            diff_text = Color.Red(f"{diff:.2f}%")
+
+        print(f"{Font.Bold('Difference')}    : {diff_text}")
+        print(Center.box(""))
