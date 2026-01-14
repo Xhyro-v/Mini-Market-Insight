c64e8cfd76ba41b5dd3d330d920247ccb6f310cc
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-14 17:16:47 +0700

    Add : adding command to chose the days trade

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import time
 from Utils.Utility import Input, Color, Center, Font
 from Core.Data_fetcher import fetch_current_price, fetch_historical_prices
 from Core.Processor import analyze_signal
@@ -10,40 +11,60 @@ CRYPTO_LIST = [
     "binancecoin"
 ]
 
+TIME = [
+    "1",
+    "7",
+    "30",
+    ]
+
 
 print(Center.box("CRYPTO SIGNAL ANALYZER"))
 
-while True:
-    print(Font.Bold("=====MENU====="))
-    print("1.Crypto")
-    print("2.keluar\n")
-    cmd = input("Pilih menu: ").strip().capitalize()
-    if cmd in ["1","Crypto"]:
-    
-        print(" ")
-        print(Center.box("CRYPTO LIST"))
-        for i, coin in enumerate(CRYPTO_LIST, 1):
-            print(f"{i}. {coin}")
-  
-        choice = int(Input.number("\nPilih Crypto (1-4): "))
-        coin_id = CRYPTO_LIST[choice - 1]
-    
-        historical_prices = fetch_historical_prices(coin_id, days=7)
-    
-        current_price = fetch_current_price(coin_id)
-        signal, avg_price, diff = analyze_signal(current_price, historical_prices)
-    
-        Output.report(
-            coin=coin_id,
-            current=current_price,
-            avg=avg_price,
-            signal=signal,
-            diff=diff
-        )
-        print("")
-    elif cmd in ["2","Keluar"]:
-        print(Color.Blue(Center.text("Program selesai")))
-        break
-    
-    else:
-        print(Color.Red(Center.text(f"There's no menu name {cmd}\n")))
+def Main():
+      while True:
+          print(Font.Bold("=====MENU====="))
+          print("1.Crypto")
+          print("2.keluar\n")
+          cmd = input("Pilih menu: ").strip().capitalize()
+          if cmd in ["1","Crypto"]:
+          
+              print(" ")
+              print(Center.box("CRYPTO LIST"))
+              for i, coin in enumerate(CRYPTO_LIST, 1):
+                  print(f"{i}. {coin}")
+        
+              choice = int(Input.number("\nPilih Crypto (1-4): "))
+              coin_id = CRYPTO_LIST[choice - 1]
+              
+              for i, TM in enumerate(TIME, 1):
+                  print(f"{i}. {TM} day")
+              
+              times = int(Input.number("\nPilih waktu: "))
+              if time == 2:
+                  times = 7
+        
+              elif time == 3:
+                  times = 30
+          
+              historical_prices = fetch_historical_prices(coin_id, days=times)
+          
+              current_price = fetch_current_price(coin_id)
+              signal, avg_price, diff = analyze_signal(current_price, historical_prices)
+          
+              Output.report(
+                  coin=coin_id,
+                  current=current_price,
+                  avg=avg_price,
+                  signal=signal,
+                  diff=diff
+              )
+              print("")
+          elif cmd in ["2","Keluar"]:
+              print(Color.Blue(Center.text("Program selesai")))
+              break
+          
+          else:
+              print(Color.Red(Center.text(f"There's no menu name {cmd}\n")))
+
+if __name__ == '__main__':
+    Main()
