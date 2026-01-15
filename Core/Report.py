9eddd7bf86bae5f6de1392331c43bdb5e6481c67
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-15 17:28:25 +0700

    Chore : Changing AVG price to real user input

--- Core/Report.py
+++ Core/Report.py
@@ -2,18 +2,18 @@ from Utils.Utility import Color, Center, Font
 
 class Output:
     @staticmethod
-    def report(coin, current, avg, signal, diff):
+    def report(coin, current, avg, signal, diff,times) -> None:
         print("\n" + Center.box("CRYPTO REPORT"))
 
-        print(f"{Font.Bold('Crypto')}        : {coin}")
-        print(f"{Font.Bold('Current Price')} : {Color.Green('$')}{Color.Green(f'{current:,.2f}')}")
-        print(f"{Font.Bold('Avg (30D)')}     : ${avg:,.2f}")
-        print(f"{Font.Bold('Signal')}        : {signal}")
+        print(f"{Font.Bold('Crypto')}         : {coin}")
+        print(f"{Font.Bold('Current Price')}  : {Color.Green('$')}{Color.Green(f'{current:,.2f}')}")
+        print(f"{Font.Bold(f'Avg {times}')}        : ${avg:,.2f}")
+        print(f"{Font.Bold('Insight')}        : {signal}")
 
         if diff >= 0:
             diff_text = Color.Green(f"+{diff:.2f}%")
         else:
             diff_text = Color.Red(f"{diff:.2f}%")
 
-        print(f"{Font.Bold('Difference')}    : {diff_text}")
+        print(f"{Font.Bold('Difference')}     : {diff_text}")
         print(Center.box(""))
