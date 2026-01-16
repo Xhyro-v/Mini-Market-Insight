6d83f4af8ad708a7e0452a3a1295c16388ec1659
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-16 21:33:03 +0700

    Feat : add command to choose menu and separate the menu

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import time
 from Utils.Utility import Input, Color, Center, Font
 from Core.Data_fetcher import fetch_current_price, fetch_historical_prices
 from Core.Processor import analyze_signal
@@ -18,7 +17,7 @@ TIME = [
     ]
 
 
-print(Center.box("CRYPTO SIGNAL ANALYZER"))
+print(Center.box(Center.text("CRYPTO SIGNAL ANALYZER")))
 
 def Main():
       while True:
@@ -27,44 +26,57 @@ def Main():
           print("2.keluar\n")
           cmd = input("Pilih menu: ").strip().capitalize()
           if cmd in ["1","Crypto"]:
-          
-              print(" ")
-              print(Center.box("CRYPTO LIST"))
-              for i, coin in enumerate(CRYPTO_LIST, 1):
-                  print(f"{i}. {coin}")
         
-              choice = int(Input.number("\nPilih Crypto (1-4): "))
-              coin_id = CRYPTO_LIST[choice - 1]
+              print("\n=====CRYPTO=MENU=====")
+              print("1.Lihat harga")
+              print("2.Buat saran keputusan\n")
+            
+              submenu = input("Pilih Submenu: ").strip()
+              if submenu in ["1"]:
+                  Output.list_crypto(CRYPTO_LIST)
+                  choice = int(Input.number("\nPilih Crypto (1-4): "))
+                  coin_id = CRYPTO_LIST[choice - 1]
+            
+                  current_price = fetch_current_price(coin_id)
+                  Output.report_info(coin_id,current_price)
               
-              for i, TM in enumerate(TIME, 1):
-                  print(f"{i}. {TM} day")
-              while True:
-                  times = int(Input.number("\nPilih waktu: "))
-                  if time == 2:
-                      times = 7
+              elif submenu in ["2"]:
+              
+                  Output.list_crypto(CRYPTO_LIST)
+                  choice = int(Input.number("\nPilih Crypto (1-4): "))
+                  coin_id = CRYPTO_LIST[choice - 1]
+                  
+                  for i, TM in enumerate(TIME, 1):
+                      print(f"{i}. {TM} day")
             
-                  elif time == 3:
-                      times = 30
-            
-                  elif time not in [1,7,30]:
-                      print(Color.Red(Center.text("Not valid")))
-          
-              avg_price = float(Input.number("Masukan harga saat beli: "))
-          
-              historical_prices = fetch_historical_prices(coin_id, days=times)
-          
-              current_price = fetch_current_price(coin_id)
-              signal, avg_price, diff = analyze_signal(current_price, historical_prices,avg_price)
-          
-              Output.report(
-                  coin=coin_id,
-                  current=current_price,
-                  avg=avg_price,
-                  signal=signal,
-                  diff=diff,
-                  times=times
-              )
-              print("")
+                  while True:
+                      times = int(Input.number("\nPilih waktu: "))
+                      if times == 2:
+                          times = 7
+                  
+                      elif times == 3:
+                          times = 30
+                          break
+                  
+                      else:
+                          print(Color.Red(Center.text("Not valid")))
+                  
+                  avg_price = float(Input.number("Masukan harga saat beli: "))
+              
+                  historical_prices = fetch_historical_prices(coin_id, days=times)
+              
+                  current_price = fetch_current_price(coin_id)
+                  signal, avg_price, diff = analyze_signal(current_price, historical_prices,avg_price)
+              
+                  Output.report_decc(
+                      coin=coin_id,
+                      current=current_price,
+                      avg=avg_price,
+                      signal=signal,
+                      diff=diff,
+                      times=times
+                  )
+                  print("")
           elif cmd in ["2","Keluar"]:
               print(Color.Blue(Center.text("Program selesai")))
               break
