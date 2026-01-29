e5abc5dae14a48bdd2a2d95bd24e945119fcd3c0
Xhyro-v <naufalazharxyz12@gmail.com>
2026-01-29 15:59:41 +0700

    fix : correct signal decision logic

--- Core/Processor.py
+++ Core/Processor.py
@@ -1,14 +1,18 @@
 import numpy as np
-from Utils.Utility import Color, Input
+from Utils.Utility import Color
 
-def analyze_signal(current_price, historical_prices, avg_price):
+def analyze_signal(current_price, avg_price):
     diff_percent = ((current_price - avg_price) / avg_price) * 100
 
-    if diff_percent > 3:
-        signal = f"{Color.Red('Consider profit')}"
-    elif diff_percent < -3:
-        signal = f"{Color.Green('Consider entry')}"
+    if diff_percent >= 7:
+        signal = Color.Red("Strong Take Profit ")
+    elif diff_percent >= 3:
+        signal = Color.Yellow("Take Profit ")
+    elif diff_percent <= -7:
+        signal = Color.Red("Strong Cut Loss ")
+    elif diff_percent <= -3:
+        signal = Color.Green("Potential Entry ")
     else:
-        signal = f"{Color.Yellow('Hold/Wait')}"
+        signal = Color.Blue("Hold / Wait ")
 
-    return signal, avg_price, diff_percent
+    return signal, diff_percent
